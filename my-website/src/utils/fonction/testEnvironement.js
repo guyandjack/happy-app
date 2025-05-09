7cc83ce9e9e2780b039f025b2d192e1e4d512362
guyandjack <guillaume-d@tutanota.com>
2025-05-09 23:52:34 +0200

    integration url render

--- my-website/src/utils/fonction/testEnvironement.js
+++ my-website/src/utils/fonction/testEnvironement.js
@@ -4,21 +4,27 @@
 const baseDevUrl = import.meta.env.VITE_BASE_DEV_URL;
 const apiDevUrl = import.meta.env.VITE_API_DEV_URL;
 
+//render
+const baseRenderUrl = import.meta.env.VITE_BASE_RENDER_URL;
+const apiRenderUrl = import.meta.env.VITE_API_RENDER_URL;
 
 //prod
 const baseProdUrl = import.meta.env.VITE_BASE_PROD_URL;
 const apiProdUrl = import.meta.env.VITE_API_PROD_URL;
 
-
-
 const mode = import.meta.env.MODE;
-
-
 
 // Fonction pour vérifier que les variables d'environnement sont bien chargées
 function validateEnvVariables() {
-  if (!baseDevUrl || !baseProdUrl || !apiDevUrl || !apiProdUrl || !mode) {
-   
+  if (
+    !baseDevUrl ||
+    !baseProdUrl ||
+    !apiDevUrl ||
+    !apiProdUrl ||
+    !mode ||
+    !baseRenderUrl ||
+    !apiRenderUrl
+  ) {
     throw new Error(
       "Une ou plusieurs variables d'environnement sont manquantes."
     );
@@ -30,27 +36,32 @@ function validateEnvVariables() {
  * and returns the appropriate URLs
  * @returns {Object} Object containing url, urlApi, and mode
  */
- function localOrProd() {
-  const isLocalhost = 
-    window.location.hostname === 'localhost' || 
-    window.location.hostname === '127.0.0.1';
-  
+function localOrProd() {
+  const isLocalhost =
+    window.location.hostname === "localhost" ||
+    window.location.hostname === "127.0.0.1";
+
+  const isRender = window.location.hostname === "happy-app.onrender.com";
+
   if (isLocalhost) {
     return {
       url: baseDevUrl,
       urlApi: apiDevUrl,
-      mode: mode
+      mode: mode,
+    };
+  } else if (isRender) {
+    return {
+      url: baseRenderUrl,
+      urlApi: apiRenderUrl,
+      mode: mode,
     };
   } else {
     return {
       url: baseProdUrl,
       urlApi: apiProdUrl,
-      mode: mode
+      mode: mode,
     };
   }
 }
-
-
-
 
 export { localOrProd };
