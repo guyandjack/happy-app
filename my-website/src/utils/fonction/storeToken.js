87f7f465ae8dbe0eb0c53f8e8fb908ce117c9724
guyandjack <guillaume-d@tutanota.com>
2025-06-10 00:35:50 +0200

    update storeToken function

--- my-website/src/utils/fonction/storeToken.js
+++ my-website/src/utils/fonction/storeToken.js
@@ -6,24 +6,26 @@
  * @returns boolean
  */
 function storeToken(responseData, jwtDecode) {
-  // Store token and user
-  localStorage.setItem("token", responseData.accessToken.toString());
-  localStorage.setItem("user", responseData.data.toString());
+  try {
+    // Store token and user
+    localStorage.setItem("token", responseData.accessToken.toString());
+    localStorage.setItem("user", responseData.data.toString());
 
-  //decoded token
-  const decodedToken = jwtDecode(responseData.accessToken);
-  const expirationTime = decodedToken.exp;
-  const currentTime = Math.floor(Date.now() / 1000);
-  console.log("expirationTime:", expirationTime);
-  console.log("currentTime:", currentTime);
+    //decoded token
+    const decodedToken = jwtDecode(responseData.accessToken);
+    const expirationTime = decodedToken.exp;
+    const currentTime = Math.floor(Date.now() / 1000);
 
-  //update time remaining
-  const timeRemaining = expirationTime - currentTime;
-  if (timeRemaining > 0) {
-    localStorage.setItem("timeRemaining", timeRemaining.toString());
-    localStorage.setItem("lastTime", new Date().getTime().toString());
-    return true;
-  } else {
+    //update time remaining
+    const timeRemaining = expirationTime - currentTime;
+    if (timeRemaining > 0) {
+      localStorage.setItem("timeRemaining", timeRemaining.toString());
+      localStorage.setItem("lastTime", new Date().getTime().toString());
+      localStorage.setItem("tokenExpiration", expirationTime.toString());
+      return true;
+    }
+  } catch (error) {
+    console.error("Error storing token:", error);
     return false;
   }
 }
