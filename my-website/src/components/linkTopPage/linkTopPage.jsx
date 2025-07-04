e82fd161bd18ad1a5bce54c312746589a4aaccf4
guyandjack <guillaume-d@tutanota.com>
2025-07-04 21:18:34 +0200

    complete link to page

--- my-website/src/components/linkTopPage/linkTopPage.jsx
+++ my-website/src/components/linkTopPage/linkTopPage.jsx
@@ -1,31 +1,60 @@
 import React, { useRef } from "react";
 import { FaArrowUp } from "react-icons/fa";
+import { FaArrowDown } from "react-icons/fa";
 import "@styles/CSS/linkTopPage.css";
 
 const LinkTopPage = () => {
-  let topPage = () => {
-    let elementTopPage = document.querySelector(".page-container");
-    if (!elementTopPage) {
-      return;
+  let elementTopPage = document.querySelector(".page-container");
+  let elementBottomPage = document.querySelector("#RC-footer");
+  let topPage = (direction) => {
+    if (direction === "up") {
+      if (!elementTopPage) {
+        return;
+      }
+      window.scrollTo({
+        top: elementTopPage.offsetTop,
+        left: elementTopPage.offsetLeft,
+        behavior: "smooth",
+      });
+    } else {
+      if (!elementBottomPage) {
+        return;
+      }
+      window.scrollTo({
+        top: elementBottomPage.offsetTop,
+        behavior: "smooth",
+      });
     }
-    window.scrollTo({
-      top: elementTopPage.offsetTop,
-      left: elementTopPage.offsetLeft,
-      behavior: "smooth",
-    });
   };
 
   return (
-    <div className="link-top-page">
-      <a
-        href="#"
-        onClick={(e) => {
-          e.preventDefault();
-          topPage();
-        }}
-      >
-        <FaArrowUp />
-      </a>
+    <div>
+      <div className="link-top-page">
+        <a
+          role="button"
+          aria-label="Vers le haut de la page"
+          href="#"
+          onClick={(e) => {
+            e.preventDefault();
+            topPage("up");
+          }}
+        >
+          <FaArrowUp />
+        </a>
+      </div>
+      <div className="link-top-page">
+        <a
+          role="button"
+          aria-label="Vers le bas de la page"
+          href="#"
+          onClick={(e) => {
+            e.preventDefault();
+            topPage("down");
+          }}
+        >
+          <FaArrowDown />
+        </a>
+      </div>
     </div>
   );
 };
